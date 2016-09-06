97faaa441fb8ad9a98172a85e82039c1b0c12fdf
jcp1016 <jcp1016@mac.com>
2016-09-06 04:41:46 +0000

    fix to lambda fn

--- aws/lambda/SendSMS.py
+++ aws/lambda/SendSMS.py
@@ -10,12 +10,17 @@
 from __future__ import print_function
 import boto3
 
-sns_client = boto3.client('sns')
+def my_handler(event, context):
+        sns_client = boto3.client('sns')
 
-msg_text = 'Tap https://s3.amazonaws.com/bob.jcp.001/index.html to see inside your car.'
+        msg_text = 'Tap https://s3.amazonaws.com/bob.jcp.001/www/alert.html to see inside your car.'
 
-response = sns_client.publish(
-    TopicArn = 'arn:aws:sns:us-east-1:146208918492:BobJCPTopic',
-                Message  = msg_text,
-                Subject  = 'Motion Detected in Hot Car!',
-                MessageStructure = 'string')
+        response = sns_client.publish(
+            TopicArn = 'arn:aws:sns:us-east-1:146208918492:BobJCPTopic',
+                        Message  = msg_text,
+                        Subject  = 'Danger!  Motion detected in your car!',
+                        MessageStructure = 'string')
+                        
+        return { 
+            'message' : response
+        }
